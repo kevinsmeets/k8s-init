4e9889f0ab9fc2dc4b17fec273bb2c65931c8e1d
Kevin Smeets <kevin.smeets@cgi.com>
2024-02-23 17:04:41 +0100

    Fomats install Kubernetes Metrics Server.

--- install-kubernetes-metrics-server.sh
+++ install-kubernetes-metrics-server.sh
@@ -11,7 +11,11 @@ is_already_installed="$(kubectl -n kube-system get deployment -l 'k8s-app in (me
 if [ ${is_already_installed:0:2} == "No" ]; then
 	echo "Installing Kubernetes Metrics Server..."
 	kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v$kubernetes_metrics_server_version/components.yaml
-	kubectl -n kube-system patch deployment metrics-server --type=json -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--kubelet-insecure-tls"}]'
+	kubectl -n kube-system \
+		patch \
+		deployment metrics-server \
+		--type=json \
+		-p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--kubelet-insecure-tls"}]'
 else
 	echo "Kubernetes Metrics Server already installed"
 fi
