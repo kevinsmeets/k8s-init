9feb14b0a0683cc5c8241ea314a1ed329b4665f0
Kevin Smeets <kevin.smeets@cgi.com>
2024-02-23 21:50:16 +0100

    Processes ShellCheck issues.

--- inits/get-kubectl-bash-alias.sh
+++ inits/get-kubectl-bash-alias.sh
@@ -9,8 +9,10 @@ if grep -q "k=kubectl" ~/.bashrc; then
   exit 1
 fi
 
-echo {# My alias for kubectl} >> ~/.bashrc
-echo {alias k=kubectl} >> ~/.bashrc
-echo {complete -o default -F __start_kubectl k} >> ~/.bashrc
+{
+	# My alias for kubectl
+	echo alias k=kubectl
+	echo complete -o default -F __start_kubectl k
+} >> ~/.bashrc
 
 echo "kubectl alias 'k' has been setup with auto completion enabled!"
