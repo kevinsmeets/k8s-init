299ae50a63c2a813a7e36cd4fe256dcfa3637408
Kevin Smeets <kevin.smeets@cgi.com>
2024-02-23 10:30:46 +0100

    Updates Kubernetes Dashboard installation.

--- install-kubernetes-dashboard.sh
+++ install-kubernetes-dashboard.sh
@@ -2,12 +2,19 @@
 
 kubernetes_dashboard_version=2.7.0
 
-is_already_installed=$( kubectl get all -n kubernetes-dashboard )
+is_already_installed="$(kubectl get all -n kubernetes-dashboard 2>&1)"
+
+# echo "$is_already_installed"
+#
+# No resources found in kubernetes-dashboard namespace.
+
 if [ ${is_already_installed:0:2} == "No" ]; then
 	echo "Installing Kubernetes Dashboard..."
 	kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v$kubernetes_dashboard_version/aio/deploy/recommended.yaml
+
+	sleep 3
 	kubectl get all -n kubernetest-dashboard
-	echo "getting things ready..."
+	echo "Getting things ready..."
 	sleep 10
 
 # Create a new ServiceAccount
@@ -18,6 +25,7 @@ metadata:
   name: admin-user
   namespace: kubernetes-dashboard
 EOF
+
 # Create a ClusterRoleBinding for the ServiceAccount
 kubectl apply -f - <<EOF
 apiVersion: rbac.authorization.k8s.io/v1
@@ -33,13 +41,19 @@ subjects:
   name: admin-user
   namespace: kubernetes-dashboard
 EOF
-	kubectl -n kubernetes-dashboard create token admin-user
+
 else
 	echo "Kubernetes Dashboard already installed"
 fi
 
+echo
+echo "Use this token to login to Kubernetes Dashboard:"
 kubectl -n kubernetes-dashboard create token admin-user --duration 12h
 
+echo
+echo "This is the Kubernetes Dashboard URL:"
 echo "http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
+
+echo "Start kubectl proxy..."
 kubectl proxy
 
