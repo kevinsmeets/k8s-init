06b5d19e8d5983b8cc9e4065801dd46652178e7d
Kevin Smeets <kevin.smeets@cgi.com>
2024-02-25 20:37:10 +0100

    Fixes typo.

--- get-k8s.sh
+++ get-k8s.sh
@@ -11,7 +11,7 @@ inits/get-kubectl-bash-auto-completion.sh
 inits/get-kubectl-bash-alias.sh
 inits/get-tmux.sh
 inits/get-yq.sh
-intis/get-kubeconform.sh
+inits/get-kubeconform.sh
 inits/get-helm.sh
 inits/get-fluxcd.sh
 inits/get-k9s.sh
