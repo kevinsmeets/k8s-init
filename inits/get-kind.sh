5d5d470dae45afc4951b242d090083c0a1604110
Kevin Smeets <kevin.smeets@cgi.com>
2024-02-25 20:40:30 +0100

    Fixes kind executable move.

--- inits/get-kind.sh
+++ inits/get-kind.sh
@@ -6,8 +6,8 @@ fi
 
 kind_version="0.21.0"
 
-curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v$kind_version/kind-linux-amd64
+curl -Lo ./kind-linux-amd64 https://github.com/kubernetes-sigs/kind/releases/download/v$kind_version/kind-linux-amd64
 
-chmod +x ./kind
-sudo mv ./kind /usr/local/bin
+chmod +x ./kind-linux-amd64
+sudo mv ./kind-linux-amd64 /usr/local/bin/kind
 kind version
