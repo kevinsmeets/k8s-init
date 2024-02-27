0d4665e5bc76a5371eaf43eaa46639b84f50d3e7
Kevin Smeets <kevin.smeets@cgi.com>
2024-02-27 12:46:52 +0100

    Updates get-kubectl-bash-auto-completion.sh

--- inits/get-kubectl-bash-auto-completion.sh
+++ inits/get-kubectl-bash-auto-completion.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-sudo apt-get install bash-completion
-
 if [ -f /etc/bash_completion.d/kubectl ]; then
   echo "kubectl auto completion already installed!"
   exit 1
 fi
 
+sudo apt-get install bash-completion
 kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
 
 echo "kubectl auto completion is installed!"
