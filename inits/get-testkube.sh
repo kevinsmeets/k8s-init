e70db86ac702462bc3493e3264b1cb9b3b972275
Kevin Smeets <kevin.smeets@cgi.com>
2024-03-02 21:36:42 +0100

    Makes sure only testkube executable is extracted.

--- inits/get-testkube.sh
+++ inits/get-testkube.sh
@@ -6,7 +6,7 @@ fi
 
 curl -LO "https://github.com/kubeshop/testkube/releases/download/v1.16.26/testkube_1.16.26_Linux_x86_64.tar.gz"
 
-tar xzf testkube_1.16.26_Linux_x86_64.tar.gz
+tar xzf testkube_1.16.26_Linux_x86_64.tar.gz kubectl-testkube
 
 sudo mv kubectl-testkube /usr/local/bin/kubectl-testkube
 sudo chmod +x /usr/local/bin/kubectl-testkube
