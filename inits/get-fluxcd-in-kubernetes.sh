6b20ba7f50ac691792967842d33da8e540b4a00d
Kevin Smeets <kevin.smeets@cgi.com>
2024-03-02 23:07:32 +0100

    Fixes wait for FluxCD deployments to be ready.

--- inits/get-fluxcd-in-kubernetes.sh
+++ inits/get-fluxcd-in-kubernetes.sh
@@ -7,6 +7,9 @@ fi
 echo Installing FluxCD in kubernetes...
 flux install
 
-kubectl -n flux-system rollout status deployment flux-system --timeout=3m
+kubectl -n flux-system rollout status deployment/helm-controller --timeout=3m
+kubectl -n flux-system rollout status deployment/kustomize-controller --timeout=3m
+kubectl -n flux-system rollout status deployment/notification-controller --timeout=3m
+kubectl -n flux-system rollout status deployment/source-controller --timeout=3m
 
 flux --version
