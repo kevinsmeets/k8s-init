51da5777a3d80405680869363dcb611568863511
Kevin Smeets <kevin.smeets@cgi.com>
2024-03-03 16:57:28 +0100

    Adds print to kind create cluster.

--- kind/create-cluster.sh
+++ kind/create-cluster.sh
@@ -4,33 +4,39 @@ if ! [ -f /usr/local/bin/kind ]; then
 	exit 1
 fi
 
-echo Is KUBECONFIG environment variable set:
+echo "Create k8s cluster using kind..."
+echo
+
+echo "Is KUBECONFIG environment variable set:"
 echo "$KUBECONFIG"
 
-echo Is a kubectl configuation file present:
+echo "Is a kubectl configuation file present:"
 ls -hal "$HOME/.kube"
 
 if [ -f "$HOME/.kube/config" ]; then
 	number_of_lines=$( wc -l < "$HOME/.kube/config" )
 	if [ "$number_of_lines" -gt 3 ]; then
-		echo A ~/.kube/config file is present, exiting in order to prevent overwriting or modification of your kubectl configuration!
+		echo "A ~/.kube/config file is present, exiting in order to prevent overwriting or modification of your kubectl configuration!"
 		exit 1
 	fi
 fi
 
+echo "docker version:"
 docker version
+echo "kind version:"
 kind version
+echo "kubectl version:"
 kubectl version
 
 cluster_name=$1
 
 if [ -z "$cluster_name" ]; then
 	echo
-	echo Please enter a name for your kubernetes cluster:
+	echo "Please enter a name for your kubernetes cluster:"
 	read -r cluster_name
 fi
 
-echo Creating kubernetes cluster named "$cluster_name" ...
+echo "Creating kubernetes cluster named $cluster_name ..."
 kind create cluster \
 	--name "$cluster_name" \
 	--config kind/two-node-cluster.yaml \
@@ -38,9 +44,9 @@ kind create cluster \
 
 rc=$?
 if [ $rc -ne 0 ]; then
-	echo Something is wrong, return code $rc for 'kind create cluster'
+	echo "Something is wrong, return code $rc for 'kind create cluster'"
 	exit 1
 fi
 
-echo Cluster has been created:
+echo "Cluster has been created:"
 kubectl cluster-info
