1182dcc1ff38b55b7fd6216963a8cc3459d9e8ad
Kevin Smeets <kevin.smeets@cgi.com>
2024-03-03 17:01:41 +0100

    Adds print.

--- inits/get-testkube-bash-auto-completion.sh
+++ inits/get-testkube-bash-auto-completion.sh
@@ -4,6 +4,8 @@ if [ -f /etc/bash_completion.d/testkube ]; then
   exit 1
 fi
 
+echo "Installing auto completion for testkube..."
+
 sudo apt-get install bash-completion
 testkube completion bash | sudo tee /etc/bash_completion.d/testkube > /dev/null
 
