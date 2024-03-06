be55aefb0cbcf7092723a380ea9b5f908cc6f526
Kevin Smeets <kevin.smeets@cgi.com>
2024-03-06 13:00:00 +0100

    Adds version and uninstall-current arguments to k9s.

--- get-k8s.sh
+++ get-k8s.sh
@@ -23,7 +23,7 @@ echo "[INIT]"; inits/get-helm.sh
 echo "[INIT]"; inits/get-helm-bash-auto-completion.sh
 echo "[INIT]"; inits/get-fluxcd.sh
 echo "[INIT]"; inits/get-fluxcd-bash-auto-completion.sh
-echo "[INIT]"; inits/get-k9s.sh
+echo "[INIT]"; inits/get-k9s.sh --version 0.32.3
 
 echo "[INIT]"; kind/create-cluster.sh k8s
 
--- inits/get-k9s.sh
+++ inits/get-k9s.sh
@@ -1,12 +1,51 @@
 #!/bin/bash
-if [ -f /usr/local/bin/k9s ]; then
-  echo "k9s already installed!"
-  exit 1
+
+uninstall_current=false
+k9s_version=""
+
+function usage {
+	echo "$0 --version <k9s version number>"
+}
+
+while [ $# -gt 0 ]; do
+	if [[ $1 == "-h" || $1 == "--help" ]]; then
+		usage
+	elif [[ $1 == "-u" || $1 == "--uninstall-current" ]]; then
+		uninstall_current=true
+	elif [[ $1 == "-v" || $1 == "--version" ]]; then
+		k9s_version="$2"
+		shift
+	else
+		echo "$0: unknown option: $1"
+		exit 1
+	fi
+	shift
+done
+
+if [[ -f /usr/local/bin/k9s && $uninstall_current == false ]]; then
+	echo "k9s already installed!"
+	exit 1
+fi
+
+function die {
+    printf "ERROR: %s\n\n" "$1"
+    exit 1
+}
+
+if [[ -z $k9s_version ]]; then
+    usage
+    die "Missing parameter --version"
+fi
+
+echo "uninstall_current": $uninstall_current
+echo "k9s_version: "$k9s_version
+
+if [[ -f /usr/local/bin/k9s && $uninstall_current == true ]]; then
+        sudo rm /usr/local/bin/k9s
+        echo "Current version uninstalled!"
 fi
 
 echo "Installing k9s..."
-
-k9s_version="0.32.0"
 
 curl -LO "https://github.com/derailed/k9s/releases/download/v$k9s_version/k9s_Linux_amd64.tar.gz"
 
