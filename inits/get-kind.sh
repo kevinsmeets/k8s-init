c3a0228269a0b0252034abe312a730e687a97594
Kevin Smeets <kevin.smeets@cgi.com>
2024-03-25 14:48:33 +0100

    Resolves ShellCheck issue.

--- inits/get-kind.sh
+++ inits/get-kind.sh
@@ -55,7 +55,7 @@ fi
 
 echo "Installing kind..."
 
-curl -Lo ./kind-linux-amd64 https://github.com/kubernetes-sigs/kind/releases/download/v$kind_version/kind-linux-amd64
+curl -Lo ./kind-linux-amd64 "https://github.com/kubernetes-sigs/kind/releases/download/v$kind_version/kind-linux-amd64"
 
 chmod +x ./kind-linux-amd64
 sudo mv ./kind-linux-amd64 /usr/local/bin/kind
